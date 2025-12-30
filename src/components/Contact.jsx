b8a8ee1b903ff74097215b846878e222397b4c26
Sohail <249952656+sohail-webdev@users.noreply.github.com>
2025-12-30 18:59:35 +0530

    feat: add hover and active transition and tweak footer links' to trigger click when clicking the label as well

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,12 +8,16 @@ const Contact = ({ ref }) => {
             Have a project in mind? <span>Send me an email or check out my code on GitHub.</span>
           </p>
           <div className="footer-links">
-            <label>
-              Email: <a href="mailto:webdev.sohail@proton.me">webdev.sohail@proton.me</a>
-            </label>
-            <label>
-              GitHub: <a href="https://github.com/sohail-webdev/">sohail-webdev</a>
-            </label>
+            <a href="mailto:webdev.sohail@proton.me" rel="noopener noreferrer">
+              <label>
+                Email: <span>webdev.sohail@proton.me</span>
+              </label>
+            </a>
+            <a href="https://github.com/sohail-webdev/" rel="noopener noreferrer" target="_blank">
+              <label>
+                GitHub: <span>sohail-webdev</span>
+              </label>
+            </a>
           </div>
         </div>
       </footer>
