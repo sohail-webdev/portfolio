f0714639323ce68d99b5cd5bce6b452b1d4ffba5
Sohail <249952656+sohail-webdev@users.noreply.github.com>
2025-12-30 11:42:22 +0530

    feat: code header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,29 @@
+import logo from "../assets/icons/logo.svg";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header-wrapper">
+      <header className="header">
+        <div className="logo-name-wrapper">
+          <img src={logo} alt="logo" draggable="false" />
+          <p className="name">Sohail</p>
+        </div>
+        <nav className="nav-links-container">
+          <ul>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#work">Work</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
 };
 
 export default Header;
