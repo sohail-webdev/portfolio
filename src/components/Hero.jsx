4ac83ece244f1b20c004451876e01093ab541a26
Sohail <249952656+sohail-webdev@users.noreply.github.com>
2025-12-30 12:06:43 +0530

    feat: code hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,22 @@
+import computer_website_icon from "../assets/icons/computer-website.svg";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="hero-wrapper">
+      <div className="hero">
+        <div className="hero-heading-sub-heading-cta">
+          <h1>
+            I <span>build</span> and <span>improve</span> landing pages
+          </h1>
+          <p>I help businesses fix outdated, slow, or unclear pages by developing clean, mobile-friendly landing pages.</p>
+          <button>View My Work</button>
+        </div>
+        <div className="hero-illustration">
+          <img src={computer_website_icon} alt="" draggable="false" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
