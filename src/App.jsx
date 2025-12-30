3c13aed2435f60c95fc1f34e0c8a7a826c4cb81f
Sohail <249952656+sohail-webdev@users.noreply.github.com>
2025-12-30 11:23:31 +0530

    feat: code ui structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
+
 function App() {
-  return;
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Work />
+      <Contact />
+    </>
+  );
 }
 
 export default App;
