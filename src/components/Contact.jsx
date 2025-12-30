1d3707ea53009a49e64b2b7df80343aa8184a707
Sohail <249952656+sohail-webdev@users.noreply.github.com>
2025-12-30 15:13:43 +0530

    feat: code footer

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,24 @@
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="footer-wrapper">
+      <footer className="footer">
+        <h2>Let's Work Together</h2>
+        <div className="footer-sub-heading-links-wrapper">
+          <p>
+            Have a project in mind? <div>Send me an email or check out my code on GitHub.</div>
+          </p>
+          <div className="footer-links">
+            <label>
+              Email: <a href="mailto:webdev.sohail@proton.me">webdev.sohail@proton.me</a>
+            </label>
+            <label>
+              GitHub: <a href="https://github.com/sohail-webdev/">sohail-webdev</a>
+            </label>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default Contact;
